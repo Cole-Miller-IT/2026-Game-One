0ab0ffecc5990d427d3fbdf464063ccae99feb76
Cole-Miller-IT <68202945+Cole-Miller-IT@users.noreply.github.com>
2026-05-26 19:48:32 -0400

    added multiple splash screens

--- game-one/boot-screen/boot_screen.gd
+++ game-one/boot-screen/boot_screen.gd
@@ -1,7 +1,11 @@
 extends Control
 
-@export var loadScene: PackedScene
-@export var splashScreen: TextureRect
+# Tutorial on this: https://www.youtube.com/watch?v=QKAuacUG0y4&list=PLEHvj4yeNfeGiG6ZJXDymk5dYBAjCGiwe&index=7
+
+@export var loadScene: PackedScene # What scene to move to after the splash screens.
+@export var splashScreenContainer: Node
+var splashScreens: Array
+
 var inTime: float = 0.5
 var fadeInTime: float = 1.5
 var pauseTime: float = 1.5
@@ -11,19 +15,29 @@ var outTime: float = 0.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	get_screens()
 	fade()
 
 # Creates a simple fade in, pause, then fade out effect.
 func fade() -> void: 
-	splashScreen.modulate.a = 0.0 # Start with the texture hidden
-	
-	var tween = self.create_tween()
-	tween.tween_interval(inTime)
-	tween.tween_property(splashScreen, "modulate:a", 1.0, fadeInTime)
-	tween.tween_interval(pauseTime)
-	tween.tween_property(splashScreen, "modulate:a", 0.0, fadeOutTime)
-	tween.tween_interval(outTime)
-	await tween.finished
+	for screen in splashScreens:
+		# Fade in, then out
+		var tween = self.create_tween()
+		tween.tween_interval(inTime)
+		tween.tween_property(screen, "modulate:a", 1.0, fadeInTime)
+		tween.tween_interval(pauseTime)
+		tween.tween_property(screen, "modulate:a", 0.0, fadeOutTime)
+		tween.tween_interval(outTime)
+		await tween.finished
 	
 	get_tree().change_scene_to_packed(loadScene)
 	
+func get_screens() -> void:
+	splashScreens = splashScreenContainer.get_children()
+	for screen in splashScreens:
+		screen.modulate.a = 0.0
+		
+# Skip screens and go straight to the next scene on any input event.
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_pressed():
+		get_tree().change_scene_to_packed(loadScene)
